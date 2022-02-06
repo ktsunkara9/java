92f731044211598f35ee68a8388620af5db7ed7f
ktsunkara9 <ktsunkara@gmail.com>
2022-02-06 23:02:42 +0530

    Added code to demonistrate Factory method pattern

---
+++ designpatterns/src/main/java/inc/skt/structural/factorymethod/Demo1.java
@@ -0,0 +1,63 @@
+package inc.skt.structural.factorymethod;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		Light tubeLight = LightFactory.getLight("tubeLight");
+		tubeLight.switchOn();
+		tubeLight.switchOff();
+		
+		Light bedLight = LightFactory.getLight("bedLight");
+		bedLight.switchOn();
+		
+	}
+
+}
+
+interface Light {
+	public abstract void switchOn();
+
+	public abstract void switchOff();
+}
+
+class TubeLight implements Light {
+
+	@Override
+	public void switchOn() {
+		System.out.println("Tubelight is Switched On");
+	}
+
+	@Override
+	public void switchOff() {
+		System.out.println("Tubelight is Switched Off");
+	}
+
+}
+
+class BedLight implements Light {
+
+	@Override
+	public void switchOn() {
+		System.out.println("BedLight is Switched On");
+	}
+
+	@Override
+	public void switchOff() {
+		System.out.println("BedLight is Switched Off");
+	}
+
+}
+
+class LightFactory {
+	
+	public static Light getLight(String name) {
+		if(name.equalsIgnoreCase("TubeLight")) {
+			return new TubeLight();
+		} else if(name.equalsIgnoreCase("BedLight")) {
+			return new BedLight();
+		} else {
+			System.out.println("The Requested Light doesn't exist");
+			return null;
+		}
+	}
+}
