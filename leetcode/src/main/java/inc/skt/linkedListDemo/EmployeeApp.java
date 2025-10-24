f22abf81135761385fd9e37771ff380f57337610
Sunkara <krishnateja.sunkara@delta.com>
2025-10-25 01:26:12 +0530

    added linkedListDemo

---
+++ leetcode/src/main/java/inc/skt/linkedListDemo/EmployeeApp.java
@@ -0,0 +1,10 @@
+package inc.skt.linkedListDemo;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Jane", "Jones", 123);
+        Employee emp2 = new Employee("John", "Doe", 4567);
+        Employee emp3 = new Employee("Mary", "Smith", 22);
+        Employee emp4 = new Employee("Mike", "Wilson", 3245);
+    }
+}
